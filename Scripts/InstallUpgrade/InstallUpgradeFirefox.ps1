7b45b1964996f3d138a24f102aabc06f56818ae7
Nath <50041416+thiZxo@users.noreply.github.com>
2025-01-22 17:10:21 +1030

    Added check & show progress

--- Scripts/InstallUpgrade/InstallUpgradeFirefox.ps1
+++ Scripts/InstallUpgrade/InstallUpgradeFirefox.ps1
@@ -1,3 +1,15 @@
-Write-Host "Starting Firefox Install..." -ForegroundColor Orange
-winget install --id=Mozilla.Firefox  -e --source winget --accept-package-agreements
-Write-Host "Firefox Install Complete" -ForegroundColor Green
+Write-Host "Starting Firefox Install..." -ForegroundColor Red
+
+$wingetCommand = "winget install --id=Mozilla.Firefox -e --source winget --accept-package-agreements"
+$process = Start-Process -FilePath "powershell.exe" -ArgumentList "-Command $wingetCommand" -NoNewWindow -PassThru
+
+$process.WaitForExit()
+
+# Check if Firefox is installed
+$firefoxInstalled = (winget list | Select-String -Pattern "Mozilla Firefox")
+
+if ($firefoxInstalled) {
+    Write-Host "Firefox Install Complete" -ForegroundColor Green
+} else {
+    Write-Host "Firefox Installation Failed. Please check for errors." -ForegroundColor Red -BackgroundColor Yellow
+}
