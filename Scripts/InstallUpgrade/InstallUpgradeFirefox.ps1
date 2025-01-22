69bd060defcb13dc5bf8266c565a6cae6c1fad46
Nath <50041416+thiZxo@users.noreply.github.com>
2025-01-22 16:55:08 +1030

    Update InstallUpgradeFirefox.ps1

--- Scripts/InstallUpgrade/InstallUpgradeFirefox.ps1
+++ Scripts/InstallUpgrade/InstallUpgradeFirefox.ps1
@@ -1,3 +1,3 @@
-Write-Host "Starting Firefox Install..." -ForegroundColor Blue
-winget install --id=Mozilla.Firefox  -e --source winget --accept-package-agreements --silent
-Write-Host "Starting Firefox Complete" -ForegroundColor Green
+Write-Host "Starting Firefox Install..." -ForegroundColor Orange
+winget install --id=Mozilla.Firefox  -e --source winget --accept-package-agreements
+Write-Host "Firefox Install Complete" -ForegroundColor Green
