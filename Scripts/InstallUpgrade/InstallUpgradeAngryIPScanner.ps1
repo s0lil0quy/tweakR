2f2bc85892458abb38e7da7359567b2b6eef2a26
Nath <50041416+thiZxo@users.noreply.github.com>
2025-01-29 01:13:33 +1030

    Adding New Installs

---
+++ Scripts/InstallUpgrade/InstallUpgradeAngryIPScanner.ps1
@@ -0,0 +1,18 @@
+$hostappname = "Angry IP Scanner"
+$wingetName = "angryziber.AngryIPScanner"
+
+Write-Host "Starting $hostappname Install..." -ForegroundColor Red
+
+$wingetCommand = "winget install --id=$wingetName -e --source winget --accept-package-agreements"
+$process = Start-Process -FilePath "powershell.exe" -ArgumentList "-Command $wingetCommand" -NoNewWindow -PassThru
+
+$process.WaitForExit()
+
+$appInstalled = (winget list | Select-String -Pattern $hostappname)
+
+if ($appInstalled) {
+    Write-Host "$hostappname Install Complete" -ForegroundColor Green
+} else {
+    Write-Host "$hostappname Install Failed. Please check for errors." -ForegroundColor Red -BackgroundColor Yellow
+
+}
